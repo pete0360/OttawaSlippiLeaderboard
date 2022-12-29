82d82c768b0de2ae8915ac1624b83712f3c8f696
Ben Peterkins <benpeterkins@Bens-MacBook-Pro.local>
2022-12-29 11:27:11 -0500

    minor bug fixes

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: 'https://https://pete0360.github.io/OttawaSlippiLeaderboard/',
+  repoPath: 'https://pete0360.github.io/OttawaSlippiLeaderboard/',
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
